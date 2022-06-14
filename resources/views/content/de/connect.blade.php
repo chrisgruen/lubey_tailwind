309784fde431aeb2c111c6f3caf10223543d5792
chrisgruen <cg@lubey.de>
2022-06-14 15:50:51 +0200

    connect (2)

--- resources/views/content/de/connect.blade.php
+++ resources/views/content/de/connect.blade.php
@@ -32,6 +32,111 @@
         </div>
     </section>
 
+    <section class="bg-gradient-to-b from-[#8E5BA0] to-[#482E51] text-white margin-to-top2 mt-0 h-128">
+        <div class="container mx-auto px-6 pt-0 pb-0 px-5">
+
+        </div>
+    </section>
+
+    <section class="container  mx-auto mt-10 px-6 pt-10 px-5">
+        <h3 class="text-5xl text-center text-blue-800 before-line2">
+            Alle Vorteile im Überblick
+        </h3>
+    </section>
+
+    <section class="container mx-auto px-6 pt-10 pb-6 px-24">
+        <div id="accordion-collapse" data-accordion="open">
+            <h2 id="accordion-collapse-heading-1">
+                <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                    <span>Accordion Flowbite 1</span>
+                    <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+                <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                    <p class="mb-2">
+                        Accordion Flowbite 1 <br />
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    </p>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-2">
+                <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+                    <span>Accordion Flowbite 2</span>
+                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+                <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                    <p class="mb-2">
+                        Accordion Flowbite 2 <br />
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    </p>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-3">
+                <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                    <span>Accordion Flowbite 3</span>
+                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
+                <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                    <p class="mb-2">
+                        Accordion Flowbite 3 <br />
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container mx-auto mt-20">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-0">
+            <div class="px-10">
+                <img src="{{asset('img/frontend/Lubey_Softwarebox_Anzug.png')}}" alt="Monitoring"/>
+            </div>
+            <div class="px-10">
+                <h2 class="text-3xl text-blue-800">
+                    <span class="font-semibold">Maßgeschneiderte Lösungen - </span> <br />
+                    So individuell wie Ihr Unternehmen
+                </h2>
+                <div class="flex items-center flex-wrap mb-20 mt-20">
+                    <div class="w-full">
+                        <div class="flex">
+                            <div class="flex-none w-14 h-14">
+                                <img src="{{asset('img/frontend/Check.png')}}" width="36" alt="Check"/>
+                            </div>
+                            <div class="text-gray-600 mb-8 pr-20">
+                                <h4 class="text-blue-800 text-lg font-semibold">Überschrift 1</h4>
+                                <p class="text-gray-600">
+                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex">
+                            <div class="flex-none w-14 h-14">
+                                <img src="{{asset('img/frontend/Check.png')}}" width="36" alt="Check"/>
+                            </div>
+                            <div class="text-gray-600 mb-8 pr-20">
+                                <h4 class="text-blue-800 text-lg font-semibold">Überschrift 2</h4>
+                                <p class="text-gray-600">
+                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex mt-8">
+                            <div class="flex-none w-14 h-14"></div>
+                            <a href="{{route('content.page',['pageID'=>'individual'])}}" class="bg-white border border-blue-800 text-blue-800  rounded mb-3 py-3 px-3 uppercase tracking-wider hover:text-white hover:bg-blue-800">
+                                Erfahren Sie mehr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- COL 3 -->
     <section class="container mx-auto px-6 mt-14 mb-14 px-0 lg:px-24">
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-0 h-full lg:h-80 text-white">
@@ -67,4 +172,23 @@
             </div>
         </div>
     </section>
+
+    <div class="container mx-auto mt-16">
+        <div class="grid grid-cols-6 gap-3 px-24 py-20">
+            <div class="col-span-4 pr-10">
+                <img src="{{asset('img/frontend/Entsorgungsunternehmen.png')}}" alt="Entsorgungsunternehmen"/>
+            </div>
+            <div class="col-span-2">
+                <h2 class="text-3xl text-blue-800">Für Entsorgungsunternehmen</h2>
+                <p class="mt-10">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                </p>
+                <div class="mt-10">
+                    <a href="{{route('content.page',['pageID'=>'individual'])}}" class="bg-white border border-blue-800 text-blue-800  rounded mb-3 py-3 px-3 uppercase tracking-wider hover:text-white hover:bg-blue-800">
+                        Erfahren Sie mehr
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
