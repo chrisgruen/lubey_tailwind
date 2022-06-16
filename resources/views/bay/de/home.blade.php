bf6955750f65ae0fb7ec1f0dc78eba2858e9ecfe
chrisgruen <cg@romonta-schach.de>
2022-06-16 13:32:43 +0200

    Newsletter responsive

--- resources/views/bay/de/home.blade.php
+++ resources/views/bay/de/home.blade.php
@@ -231,7 +231,7 @@
                 </div>
     		</div>
     		<div class="w-full lg:w-2/4">
-        		<img src="{{asset('img/frontend/Lubey_Trade_Cloud.png')}}" alt="Cloud" width="500"/>
+        		<img class="mx-auto" src="{{asset('img/frontend/Lubey_Trade_Cloud.png')}}" alt="Cloud" width="500"/>
     		</div>
     		<div class="w-full lg:w-1/4">
         		<div class="flex mt-8">
@@ -385,17 +385,21 @@
 
     <section class="bg-gradient-to-b from-[#4FAAE3] to-blue-800 text-white">
 		<div class="container mx-auto px-6 pt-10 pb-6 px-5">
-    		<h3 class="text-3xl text-center before-line">NEWSLETTER</h3>
+    		<div class="flex items-center py-4">
+        		<div class="flex-grow h-px bg-white"></div> 
+        		<span class="flex-shrink text-3xl font-semibold px-6">NEWSLETTER</span>
+                <div class="flex-grow h-px bg-white"></div>
+            </div>
     		<div class="flex flex-wrap mx-auto items-baseline mt-12 w-full lg:w-10/12">
     			<div class="w-full md:w-2/5 px-10 mb-10 lg:mb-0">
     				Ihr E-Mail Adresse<br />
-    				<input type="text" value="tbone" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md "/>
+    				<input type="text" value="tbone" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-gray-700" placeholder="E-Mail-Adresse eintragen... "/>
                 	<div class="flex mt-4">
             			<div class="flex-none">
                       		<input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
                       	</div>
                       	<p class="text-white text-xs">
-    						Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+    						Mit meiner Anmeldung zu dem personalisierten Newsletter willige ich ein, dass personalisierte Nutzungsprofile erstellt werden, um die Ansprache, Informationen und Angebote besser auf meine persönlichen Interessen anzupassen.
                       	</p>
                     </div>
                     <div class="flex mt-4">
@@ -410,72 +414,75 @@
     			</div>
     			<div class="w-full md:w-3/5 px-10">
     				<div>Lubey Newsletter mit auf meine Bedürfnisse zugeschnittenen <br />Informationen rund um die Themen</div>
-    				<div class="flex flex-wrap space-x-4 mt-5">
-    					<div class="flex-none w-24">
-    						<span>Lubey</span><br />
-    						<span class="text-lg">Produkte</span>
-    					</div>
-    					<div class="flex-initial w-48">
-        					<div class="form-check">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Connect
-                              </label>
-                            </div>
-                            <div class="form-check mr-10">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Trade
-                              </label>
-                            </div>
-    					</div>
-    					<div class="flex-initial">
-        					<div class="form-check">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Digitale Lösungen
-                              </label>
-                            </div>
-                            <div class="form-check mr-10">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Consulting
-                              </label>
-                            </div>
-    					</div>
-    				</div>
-    				<div class="flex flex-wrap space-x-4 mt-5">
-    					<div class="flex-none w-24">
-    						<span>Lubey</span><br />
-    						<span class="text-lg">Trade</span>
-    					</div>
-    					<div class="flex-initial w-48">
-        					<div class="form-check">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Städte & Kommunen
-                              </label>
-                            </div>
-                            <div class="form-check mr-10">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Handel
-                              </label>
-                            </div>
+    				   				
+    				<div class="overflow-x-scroll">
+                        <div class="w-[32rem] flex space-x-4 mt-5">
+                        	<div class="flex-none w-24">
+                        		<span>Lubey</span><br />
+                        		<span class="text-lg">Produkte</span>
+                        	</div>
+                        	<div class="flex-initial w-48">
+                        		<div class="form-check">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Connect
+                        		  </label>
+                        		</div>
+                        		<div class="form-check mr-10">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Trade
+                        		  </label>
+                        		</div>
+                        	</div>
+                        	<div class="flex-initial">
+                        		<div class="form-check">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Digitale Lösungen
+                        		  </label>
+                        		</div>
+                        		<div class="form-check mr-10">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Consulting
+                        		  </label>
+                        		</div>
+                        	</div>
                         </div>
-                        <div class="flex-initial">
-        					<div class="form-check">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Industrie
-                              </label>
-                            </div>
-                            <div class="form-check mr-10">
-                              <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label inline-block text-white" for="flexCheckDefault">
-                                Entsorger
-                              </label>
-                            </div>
+                        <div class="w-[32rem] flex space-x-4 mt-5">
+                        	<div class="flex-none w-24">
+                        		<span>Lubey</span><br />
+                        		<span class="text-lg">Trade</span>
+                        	</div>
+                        	<div class="flex-initial w-48">
+                        		<div class="form-check">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Städte & Kommunen
+                        		  </label>
+                        		</div>
+                        		<div class="form-check mr-10">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Handel
+                        		  </label>
+                        		</div>
+                        	</div>
+                        	<div class="flex-initial">
+                        		<div class="form-check">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Industrie
+                        		  </label>
+                        		</div>
+                        		<div class="form-check mr-10">
+                        		  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                        		  <label class="form-check-label inline-block text-white" for="flexCheckDefault">
+                        			Entsorger
+                        		  </label>
+                        		</div>
+                        	</div>
                         </div>
     				</div>
     			</div>
