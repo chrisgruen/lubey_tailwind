bd34b34144f6263233fc919f16743a13ed72985b
chrisgruen <cg@romonta-schach.de>
2022-06-16 15:15:02 +0200

    connect texte + responsive

--- resources/views/content/de/connect.blade.php
+++ resources/views/content/de/connect.blade.php
@@ -8,20 +8,29 @@
 		</div>
     </section>
 
-    <section class="container relative mx-auto px-6 mt-10 xl:-mt-48 z-10">
-        <div class="flex items-center flex-wrap mb-20">
-            <div class="w-full md:w-1/2 px-14">
+    <section class="container relative mx-auto px-6 mt-10 xl:-mt-60 z-10">
+        <div class="flex items-center flex-wrap mb-20 xl:pt-36">
+            <div class="w-full md:w-1/2 px-5 xl:px-14 mb-16  xl:mb-0" >
 				<h3 class="text-4xl text-blue-800 font-light leading-10">
 					<span class="font-semibold">Neues Zeitalter</span> - <br />
 					Digitalisierung der Entsorgung und Abrechnung
 				</h3>
 				<p class="mt-5">
-					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-					At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+					„Connect“ digitalisiert die direkte Entsorgung und deren Abrechnung. 
+					Durch die Automatisierung kann die Zusammenarbeit 
+					zwischen Entsorgern und Erzeuger vereinfacht 
+					und weitaus effizienter gemacht werden.
+				</p>
+				<p class="mt-5">
+					Connect ist in der Lage alle Entsorgungsaufträge, 
+					die mit Lubey Trade zustande gekommen sind, verwalten, auslösen und
+ 					automatisiert abrechnen zu können.  
+					Zusätzlich sind Erzeuger und Entsorger direkt über die Lösung
+					vernetzt und können auf einfachem Wege kommunizieren.  
 				</p>
 				<div class="mt-10">
-    				<a href="#" class="bg-[#51ADE6] border border-blue-300 text-white rounded mb-3 py-3 px-3 uppercase tracking-wider hover:text-white hover:bg-blue-800">
-                        Jetzt Beratungstermin vereinbaren!
+    				<a href="#" class="bg-[#51ADE6] border border-blue-300 text-white rounded mb-3 py-3 px-7 text-lg hover:text-white hover:bg-blue-800">
+                        <span class="font-semibold">Jetzt</span> Beratungstermin vereinbaren!
                     </a>
                 </div>
             </div>
@@ -32,58 +41,119 @@
     </section>
 
     <section class="bg-gradient-to-b from-[#8E5BA0] to-[#482E51] text-white margin-to-top2 mt-0 h-128">
-        <div class="container mx-auto px-6 pt-0 pb-0 px-5">
-
+        <div class="container mx-auto px-6 pt-10 pb-0 px-5">
+			<h2 class="text-3xl md:text-5xl text-center">So funktioniert's</h2>
+			<p class="text-center">
+				Erfahren Sie anhand eines kurzen Videos wie einfach heute schon digitales und intelligentes Abfallmanagement geht
+			</p>
         </div>
     </section>
 
     <section class="container  mx-auto mt-10 px-6 pt-10 px-5">
-        <h3 class="text-5xl text-center text-blue-800 before-line2">
-            Alle Vorteile im Überblick
-        </h3>
+    	<div class="flex items-center py-4">
+    		<div class="flex-grow h-px bg-gray-400"></div> 
+    		<span class="flex-shrink text-blue-800 text-3xl lg:text-5xl px-6">Alle Vorteile im Überblick</span>
+            <div class="flex-grow h-px bg-gray-400"></div>
+        </div>
     </section>
 
-    <section class="container mx-auto px-6 pt-10 pb-6 px-24">
+    <section class="container mx-auto px-6 pt-10 pb-6 px-5 lg:px-24">
         <div id="accordion-collapse" data-accordion="open">
             <h2 id="accordion-collapse-heading-1">
                 <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-                    <span>Accordion Flowbite 1</span>
+                    <span>
+                    	Für jedes Unternehmen die richtige Struktur<br />
+                    	Digitale Vernetzung zwischen den Geschäfts-Parteien
+                	</span>
                     <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </h2>
             <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
                 <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                     <p class="mb-2">
-                        Accordion Flowbite 1 <br />
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        Connect ist in der Lage alle Entsorgungsaufträge, die mit Trade zustande gekommen sind, verwalten, auslösen und automatisiert abrechnen zu können.  
+                        Zusätzlich sind Erzeuger und Entsorger direkt über die Lösung vernetzt und können auf einfachem Wege kommunizieren.  
+                    </p>
+                    <p class="mb-2">
+                        Optional kann dies so weit ausgebaut werden, dass durch intelligente und flexible Abholauslösungen und Vernetzung mit Fahrern des Entsorgers oder seiner beauftragten Logistikpartnern Leerfahrten, bzw. Umsonst-Fahrten ganzer Fahrzeugflotten vermieden werden können.   
                     </p>
                 </div>
             </div>
             <h2 id="accordion-collapse-heading-2">
                 <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
-                    <span>Accordion Flowbite 2</span>
+                    <span>Alle Entsorgungsaufträge im Blick behalten</span>
                     <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </h2>
             <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
                 <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
                     <p class="mb-2">
-                        Accordion Flowbite 2 <br />
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        Alle wichtigen Daten des über Trade geschlossenen Vertrages werden automatisch auf Connect übertragen. Mit Connect kann man direkt seine Entsorgungsaufträge einsehen, den aktuellen Status der Entsorgung prüfen, Entsorgungsrechnungen bzw. -gutschriften aufrufen und übersichtlich darstellen lassen.
                     </p>
                 </div>
             </div>
             <h2 id="accordion-collapse-heading-3">
                 <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
-                    <span>Accordion Flowbite 3</span>
+                    <span>
+                    	Transparente Statistiken für eine bessere Auswertung<br />
+                    	Vollelektronisch Rechnungen bzw. Gutschriften übermitteln
+                	</span>
                     <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </h2>
             <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
                 <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
                     <p class="mb-2">
-                        Accordion Flowbite 3 <br />
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        Rechnungen und Gutschriften können entweder direkt in Connect erstellt oder mit bereits vorhandenen Softwarelösungen an die Lösung und damit an den Zahlungs- oder Gutschriften-Empfänger übermittelt werden. Durch die Automatisierung ist keine manuelle Erfassung mehr nötig, was kürzere Bearbeitungszeiten mit sich bringt und die Effizienz steigert.
+                    </p>
+                    <p class="mb-2">
+                        Der Erzeuger kann mit Connect seine administrativen Prozesse und Belegflüsse vereinfachen und vor allem vereinheitlichen, in dem alle mit Trade abgewickelten Geschäftsabschlüsse als standardisierte E-Rechnung im X-Format, bzw. ZUGFeRD-Format vom Entsorgungsunternehmen künftig eingehen.
+                    </p>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-4">
+                <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-4" aria-expanded="false" aria-controls="accordion-collapse-body-4">
+                    <span>
+                    	Per Knopfdruck Entsorgungen auslösen
+                	</span>
+                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-4" class="hidden" aria-labelledby="accordion-collapse-heading-4">
+                <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                    <p class="mb-2">
+                       	Connect hilft dabei Informationsverluste und Missverständnisse bei der Auslösung von Entsorgungsvorgängen zu vermeiden. Mit der Lösung wird dies dahingehend vereinfacht, dass mit nur einem Klick die Entsorgung für einen Abfall ausgelöst werden kann.
+                    </p>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-5">
+                <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-5" aria-expanded="false" aria-controls="accordion-collapse-body-5">
+                    <span>
+                    	Vollelektronisch Rechnungen bzw. Gutschriften erstellen, übermitteln und verarbeiten<br />
+                    	Für jedes Unternehmen die richtige Struktur
+                	</span>
+                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-5" class="hidden" aria-labelledby="accordion-collapse-heading-5">
+                <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                    <p class="mb-2">
+                       	Mit der Lösung kann die individuelle Struktur des Unternehmens abgebildet werden. So können Regionen oder Werke mit Mitarbeitern zugeordnet werden.
+                    </p>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-6">
+                <button type="button" class="flex justify-between items-center p-5 w-full text-xl text-left text-gray-500 border border-gray-200 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-6" aria-expanded="false" aria-controls="accordion-collapse-body-6">
+                    <span>
+                    	Mehr Sicherheit durch Überprüfung von Rechnungen, Gutschriften und Belegen
+                	</span>
+                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-6" class="hidden" aria-labelledby="accordion-collapse-heading-6">
+                <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                    <p class="mb-2">
+                       	Connect übernimmt die automatisierte Plausibilitätsprüfung von allen Rechnungen bzw. Gutschriften. Sollten vertragliche Abweichungen festgestellt werden, so wird dies mitgeteilt. Darüber hinaus werden stichprobenartige Prüfungen der Rechnungen, Gutschriften und Belege durchgeführt. Je nach Bedarf kann Connect entweder nur die Dokumentenprüfung mit Übersendung einer monatlichen Zahlungsdatei übernehmen oder eine automatische Verbuchung beim Unternehmen durchführen.
                     </p>
                 </div>
             </div>
@@ -96,7 +166,7 @@
                 <img src="{{asset('img/frontend/Lubey_Softwarebox_Anzug.png')}}" alt="Monitoring"/>
             </div>
             <div class="px-10">
-                <h2 class="text-3xl text-blue-800">
+                <h2 class="text-3xl text-blue-800 mt-16 lg:mt-0">
                     <span class="font-semibold">Maßgeschneiderte Lösungen - </span> <br />
                     So individuell wie Ihr Unternehmen
                 </h2>
@@ -178,7 +248,9 @@
                 <img src="{{asset('img/frontend/Entsorgungsunternehmen.png')}}" alt="Entsorgungsunternehmen"/>
             </div>
             <div class="col-span-2">
-                <h2 class="text-3xl text-blue-800">Für Entsorgungsunternehmen</h2>
+                <h2 class="text-3xl text-blue-800 mt-16 lg:mt-0">
+                	Für Entsorgungsunternehmen
+            	</h2>
                 <p class="mt-10">
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                 </p>
