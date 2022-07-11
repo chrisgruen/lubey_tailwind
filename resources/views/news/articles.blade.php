880c396a3a1eecb667ec0ce206db81bc80bc701d
chrisgruen <cg@lubey.de>
2022-07-11 15:57:09 +0200

    News sort by

--- resources/views/news/articles.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.backend_tailwind')
-@section('title') News-Articles @endsection
-@section('content')
-    <section class="container mx-auto my-5 p-5">
-        <h1 class="text-3xl font-semibold">News-Articles</h1>
-    </section>
-@endsection
